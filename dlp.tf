a142bfecf108c11b936a414fb7f1397398e56c8d
Savita Pal <savitap@ad.corp.google.com>
2021-11-01 23:09:39 +0530

    Committed

---
+++ dlp.tf
@@ -0,0 +1,36 @@
+provider "google" {
+
+}
+
+resource "google_data_loss_prevention_job_trigger" "savita_demo_1" {
+    parent = "airline1-sabre-wolverine"
+    description = "Description"
+    display_name = "Displayname"
+
+    triggers {
+        schedule {
+            recurrence_period_duration = "86400s"
+        }
+    }
+
+    inspect_job {
+        inspect_template_name = "dlp_demo"
+        actions {
+            save_findings {
+                output_config {
+                    table {
+                        project_id = "xxxxxx-xxx-xxxxxxx"
+                        dataset_id = "dlp_demo"
+                    }
+                }
+            }
+        }
+        storage_config {
+            cloud_storage_options {
+                file_set {
+                    url = "gs://my_bucket_df/"
+                }
+            }
+        }
+    }
+}
